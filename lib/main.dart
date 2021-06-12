8082b3453015aa55d09bc80a44f12ffc4b24bdf5
Jaksa Malisic <jaksa.malisic@gmail.com>
2021-06-12 17:00:39 +0200

    Add Text and Background

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Jaksm MiCard',
       theme: ThemeData(
         primarySwatch: Colors.teal,
+        fontFamily: 'JetBrainsMono',
       ),
       home: HomeScreen(),
     );
@@ -23,19 +24,73 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
+      body: Stack(
+        children: [
+          Container(
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/background.jpeg'),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                  Colors.amber.withOpacity(0.4),
+                  Colors.red.withOpacity(0.9),
+                ])),
+          ),
+          Content(),
+        ],
+      ),
+    );
+  }
+}
+
+class Content extends StatelessWidget {
+  const Content({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 64,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
                 radius: 60,
                 backgroundImage: AssetImage('assets/images/jaksa.jpeg'),
                 backgroundColor: Colors.white,
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Jakša Mališić',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Oi',
+                fontSize: 32,
+              ),
+            ),
+            Text(
+              'Mobile App Developer',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            )
+          ],
         ),
       ),
     );
