e587b47e546ed40e764ce4f50429048d5084d749
Jaksa Malisic <jaksa.malisic@gmail.com>
2021-06-12 15:08:12 +0200

    Complete layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,33 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(50),
-          margin: EdgeInsets.all(20),
-          child: Text('Hello World'),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.yellow,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.green,
+                  width: 100,
+                  height: 100,
+                )
+              ],
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+            )
+          ],
         ),
       ),
     );
